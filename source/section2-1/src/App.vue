0deb8b390c5573c33c48b297d93b01219763c2b1
alex-toy <alexei.80@hotmail.fr>
2024-01-06 11:02:06 +0100

    progress

--- source/section2-1/src/App.vue
+++ source/section2-1/src/App.vue
@@ -1,81 +1,31 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
-</template>
-
-<style>
-@import './assets/base.css';
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-}
-</style>
+    <section>
+      <h2>My Friends</h2>
+      <ul>
+        <friend-contact v-for="friend in friends" :key="friend.id" :friend="friends[0]"></friend-contact>
+      </ul>
+    </section>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        friends: [
+          {
+            id: "manuel",
+            name: "Manuel Lorenz",
+            phone: "0123 45678 90",
+            email: "manuel@localhost.com",
+          },
+          {
+            id: "julie",
+            name: "Julie Jones",
+            phone: "0987 654421 21",
+            email: "julie@localhost.com",
+          },
+        ],
+      };
+    },
+  };
+  </script>
---
+++ source/section2-1/src/components/FriendContact.vue
@@ -0,0 +1,39 @@
+<template>
+    <li>
+      <h2>{{ friend.name }}</h2>
+      <button @click="toggleDetails">Show Details</button>
+      <ul v-if="detailsAreVisible">
+        <li>
+          <strong>Phone:</strong>
+          {{ friend.phone }}
+        </li>
+        <li>
+          <strong>Email:</strong>
+          {{ friend.email }}
+        </li>
+      </ul>
+    </li>
+  </template>
+  
+  <script>
+  export default {
+    props:{
+        friend: {
+          id: {type: String},
+          name: {type: String},
+          phone: {type: String},
+          email: {type: String},
+        },
+    },
+    data() {
+      return {
+        detailsAreVisible : false
+      };
+    },
+    methods: {
+      toggleDetails() {
+        this.detailsAreVisible = !this.detailsAreVisible;
+      }
+    }
+  };
+  </script>

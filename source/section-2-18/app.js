6dd0cb9696a8a6f6d4a5477301158a8056f21fd9
alex-toy <alexei.80@hotmail.fr>
2024-02-04 11:14:03 +0100

    styling

--- source/section-2-18/app.js
+++ source/section-2-18/app.js
@@ -1,7 +1,23 @@
 let vm = Vue.createApp({
     data(){
         return {
-            isPurple : false
+            isPurple : false,
+            selectedColor : '',
+            size: 150,
+            rotate: 0,
         };
+    },
+    computed:{
+        circleClasses(){
+            return { purple : this.isPurple };
+        }
+    },
+    methods:{
+        rotateClockwiseBy15(){
+            this.rotate = (this.rotate + 15)%360;
+        },
+        rotateCounterClockwiseBy15(){
+            this.rotate = (this.rotate - 15)%360;
+        },
     }
 }).mount('#app')

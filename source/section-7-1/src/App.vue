3a1d10206438e44e4af8d3e37870ead07cf97040
alex-toy <alexei.80@hotmail.fr>
2024-02-14 22:10:27 +0100

    animations

--- source/section-7-1/src/App.vue
+++ source/section-7-1/src/App.vue
@@ -1,8 +1,19 @@
-<template></template>
+<template>
+  <button type="button" @click="flag=!flag">Toggle</button>
+
+  <transition name="fade">
+    <h2 v-if="flag">Hello World</h2>
+  </transition>
+</template>
 
 <script>
 export default {
-  name: "App"
+  name: "App",
+  data(){
+    return {
+      flag: false,
+    }
+  }
 }
 </script>
 

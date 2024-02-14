307b5ec0418e5da11bc5acd46171e595e470f1b0
alex-toy <alexei.80@hotmail.fr>
2024-02-14 22:01:05 +0100

    keep alive

---
+++ source/dynamic-components/src/components/About.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <h1>About</h1>
+</template>
+
+<script>
+export default {
+    unmounted(){
+        console.log("About component unmounted")
+    },
+}
+</script>
+
+<style lang="">
+    
+</style>
---
+++ source/dynamic-components/src/components/Home.vue
@@ -0,0 +1,21 @@
+<template lang="">
+    <h1>Home</h1>
+</template>
+
+<script>
+export default {
+    unmounted(){
+        console.log("Home component unmounted")
+    },
+    activated(){
+        console.log("Home component activated")
+    },
+    deactivated(){
+        console.log("Home component deactivated")
+    }
+}
+</script>
+
+<style lang="">
+    
+</style>

162bd719eadf9dfad57d2b3c6cb84a3950d0aecb
alex-toy <alexei.80@hotmail.fr>
2024-02-15 20:41:25 +0100

    music

--- source/section-7-1/src/App.vue
+++ source/section-7-1/src/App.vue
@@ -1,9 +1,14 @@
 <template>
   <button type="button" @click="flag=!flag">Toggle</button>
 
-  <transition name="fade">
-    <h2 v-if="flag">Hello World</h2>
-  </transition>
+<transition name="fade" mode="out-in">
+  <h2 v-if="flag" key="main">Hello World</h2>
+  <h2 v-else="flag" key="secondary">Another Hello World</h2>
+</transition>
+
+<transition name="zoom">
+  <h2 v-if="flag" key="main">Hello</h2>
+</transition>
 </template>
 
 <script>
@@ -18,16 +23,26 @@ export default {
 </script>
 
 <style>
+.h2 {
+  width: 400px;
+  padding: 20px;
+  margin: 20px;
+}
+
 .fade-enter-from {
   opacity: 0;
 }
 
 .fade-enter-active {
-  transition: all 0.25s linear;
+  transition: all 1s linear;
 }
 
 .fade-leave-to {
-  transition: all 0.25s linear;
+  transition: all 1s linear;
   opacity: 0;
+}
+
+@keyframes zoom-in {
+  
 }
 </style>
